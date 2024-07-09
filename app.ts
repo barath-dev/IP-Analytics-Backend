8cb20e8d3ff22b92663a6eadf36350c1701bd803
Barathwaj <barathwaj1153@gmail.com>
2024-07-09 23:39:03 +0530

    try1 without docker

--- app.ts
+++ app.ts
@@ -3,29 +3,34 @@ import axios from "axios";
 import { RecordModel } from "./models/record.model";
 import { randomUUID } from "crypto";
 
-const https = require('https');
+const https = require('http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const  cert = require('fs').readFileSync("rootCA.crt");
 const key = require('fs').readFileSync("rootCA.key");
+const dotenv = require('dotenv');
 
 
 
 //create a https server using key and cert 
-const server = https.createServer({key: key, cert: cert }, app);
+const server = https.createServer( app);
 
 
 app.use(cors());
 app.use(bodyParser.json());
+dotenv.config();
+
 
 app.post('/', async (req:Request, res:Response) => {
+
+    console.log("Saving record");
 
     try {
         fetchIPDetails().then(async (data): Promise<void> => {
 
-            const response = await fetch(`${process.env.DBURL}collections/IP_Details/records?ip=${data.ip}`, {
+            const response = await fetch(`${process.env.DBURL}collections/IP_Details/records`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -35,20 +40,22 @@ app.post('/', async (req:Request, res:Response) => {
             const records = await response.json();
 
             let present = false;
+            let rec = null;
 
             for (const  record of records.items) {
                 if(record.ip === data.ip){
                     present = true;
-                    break;
+                    rec = record;
                 }
             }
 
             if (present) {
-                const record = records.items[0];
+                console.log("Record found");
+                const record = rec;
                 record.visit_count += 1;
                 //update the record in the database
                 await fetch(`${process.env.DBURL}collections/IP_Details/records/${record.id}`, {
-                    method: 'PUT',
+                    method: 'PATCH',
                     headers: {
                         'Content-Type': 'application/json',
                         "Authorization": `Bearer ${process.env.POCKETBASE_TOKEN}`,
@@ -61,6 +68,7 @@ app.post('/', async (req:Request, res:Response) => {
                 });
 
             }else{
+                console.log("Record not found");
             let record = new RecordModel(
                 randomUUID().toString().substring(0, 15),
                 data.ip,
@@ -143,35 +151,47 @@ app.get("/stats",(req:Request,res:Response)=>{
             "Authorization": `Bearer ${process.env.POCKETBASE_TOKEN}`,
         }, 
       }).then(async (response) => {
+
+        console.log(response.status);
+
         let data = await response.json();
+
+        if (data.length === 0) {
+            res.status(200).send("No records found");
+            return;
+        }
+
         data = data.items;
-        const countryStats = data.reduce((acc: any, item: any) => {
+
+        console.log(data);
+        
+        const countryStats = data?.reduce((acc: any, item: any) => {
             acc[item.country] = acc[item.country] ? acc[item.country] + item.visit_count : 1;
             return acc;
           }, {});
         
-          const orgStats = data.reduce((acc: any, item: any) => {
+          const orgStats = data?.reduce((acc: any, item: any) => {
             acc[item.org] = acc[item.org] ? acc[item.org] + 1 : 1;
             return acc;
           }, {});
 
-          const browserStats = data.reduce((acc: any, item: any) => {
+          const browserStats = data?.reduce((acc: any, item: any) => {
             acc[item.browser] = acc[item.browser] ? acc[item.browser] + 1 : 1;
             return acc;
           }, {});
 
-            const osStats = data.reduce((acc: any, item: any) => {
+            const osStats = data?.reduce((acc: any, item: any) => {
                 acc[item.os] = acc[item.os] ? acc[item.os] + 1 : 1;
                 return acc;
             }, {});
 
-          const deviceStats = data.reduce((acc: any, item: any) => {
+          const deviceStats = data?.reduce((acc: any, item: any) => {
             acc[item.device] = acc[item.device] ? acc[item.device] + 1 : 1;
             return acc;
           }, {});
 
             const overallStats = {
-                totalRecords: data.length,
+                totalRecords: data?.length,
                 totalCountries: Object.keys(countryStats).length,
                 totalOrgs: Object.keys(orgStats).length,
                 totalBrowsers: Object.keys(browserStats).length,
